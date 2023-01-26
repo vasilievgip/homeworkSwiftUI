80ce36016fa5472c0d1de1a5138d739a793a0106
vasilievgip <vasilievgip@yandex.ru>
2023-01-26 09:24:57 +0300

    Initial Commit

---
+++ homeworkSwiftUI/homeworkSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  homeworkSwiftUIApp.swift
+//  homeworkSwiftUI
+//
+//  Created by Андрей Васильев on 26.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct homeworkSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
